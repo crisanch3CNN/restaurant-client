34bf5a174a242b66327f973d7863332d1605f327
crisanch <cristian.sanchez@triciclos.cl>
2021-12-15 18:00:07 -0300

    feat(login): add view

--- App.js
+++ App.js
@@ -1,21 +1,66 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Alert } from 'react-native';
+import { Provider as PaperProvider, TextInput, Button } from 'react-native-paper';
+
 
 export default function App() {
+
+  const [userEmail, setUserEmail] = useState('')
+  const [userPassword, setUserPassword] = useState('')
+
+  const _submitLogin = () => {
+    if (userEmail.length < 5) {
+      Alert.alert("Por favor, ingrese un correo electrónico");
+      return;
+    }
+    if (userPassword.length < 5) {
+      Alert.alert("Por favor, ingrese su contraseña");
+      return;
+    }
+
+    console.log(userEmail);
+    console.log(userPassword);
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider>
+      <View style={styles.container}>
+        <TextInput
+          mode="outlined"
+          label="Email"
+          value={userEmail}
+          onChangeText={(t) => setUserEmail(t)}
+          style={styles.textInput}
+        />
+        <TextInput
+          mode="outlined"
+          label="Password"
+          secureTextEntry={true}
+          value={userPassword}
+          onChangeText={(t) => setUserPassword(t)}
+          style={styles.textInput}
+        />
+        <Button contentStyle={styles.submitBtnContent} style={styles.submitBtn} mode="contained" onPress={_submitLogin}>
+          Iniciar Sesión
+        </Button>
+      </View>
+    </PaperProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    width: "100%",
     backgroundColor: '#fff',
-    alignItems: 'center',
+    alignItems: 'stretch',
     justifyContent: 'center',
+    marginHorizontal: 16,
+    marginVertical: 32,
   },
+  textInput: { marginBottom: 8, width: "100%" },
+  submitBtn: { marginTop: 16, width: "100%" },
+  submitBtnContent: { paddingVertical: 8 }
+
 });
